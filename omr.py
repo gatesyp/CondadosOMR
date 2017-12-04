613fcad820021689fc4b0d10f70282d4fe7bcb9d
Colin Frame <cjframe1@gmail.com>
2017-12-03 23:55:00 -0500

    Removed some redundancy and added additional test image

--- omr.py
+++ omr.py
@@ -8,14 +8,14 @@ menuItems = ['Corn Tortilla', 'Flour Tortilla','Goody-Goody Tortilla','Ju-Ju Tor
     'Special Protein of the Month','Lettuce','Tomatoes','Cilantro+Onions','Jicama+Cabbage Slaw','Red Cabbage','Pickled Red Onions','Queso Fresco','Middlefield Smoked Cheddar','Chihuahua',
     'Corn Salsa w/ Tomatoes+Peppers+Onions','Pineapple Salsa w/ Tomatoes+Pepper+Onions','Salsa Verde','Salsa Roja','Chipotle Crema','Chipotle Honey','Mexican Chimichuri','Cilantro/Lime Aioli',
     'Habenero/Mango BBQ','Condado Secret Taco Sauce','Ghost Pepper']
-    
+
 menuSides = ['Rice','Black Beans','Picked Jalapenos','Tradional Gauc', 'Sour Cream','Bacon Refried Beans']
 
 tacoone = []
 tacotwo = []
 sides = []
 
-answers = cv2.imread('./imgs/answer.jpg')
+answers = cv2.imread('./imgs/answer2.jpg')
 answersgray = cv2.cvtColor(answers, cv2.COLOR_BGR2GRAY)
 answersblurred = cv2.GaussianBlur(answersgray, (5, 5), 0)
 
@@ -49,8 +49,6 @@ for c in cnts:
 	if w >= 35 and h >= 35 and w <= 46 and h <= 46 and ar >= 0.95 and ar <= 1.05:
 		questionCnts.append(c)
 
-cv2.drawContours(image, questionCnts, -1, (0,255,0), 3)
-cv2.imwrite('./imgs/test.jpg', image)
 
 #sort bubbles from top to bottom,we do not know how they are listed horizontally
 questionCnts = contours.sort_contours(questionCnts, method="top-to-bottom")[0]
@@ -64,15 +62,15 @@ for (i, c) in enumerate(questionCnts):
 	# draw bubbles onto filled in sheet one at a time
 	mask = np.zeros(answersthresh.shape, dtype="uint8")
 	cv2.drawContours(mask, [c], -1, 255, -1)
-	
+
 	# compute the total number of filled in pixels inside bounding bubble
 	mask = cv2.bitwise_and(answersthresh, answersthresh, mask=mask)
 	total = cv2.countNonZero(mask)
-	
+
 	# logic to discern if bubbles are to the left or right, and applying
 	# the menu items from each loaction to the respective taco
-	print(total)
-	if total > 750:
+
+	if total > 550:
 		if (i+1) < dif:
 			if i % 2 == 0 or i == 0:
 				if questionCnts[i][0][0][0] < questionCnts[i+1][0][0][0]:
@@ -92,7 +90,7 @@ for (i, c) in enumerate(questionCnts):
 					else:
 						tacoone.append(menuItems[int((i-1)/2)])
 				else:
-					if i == 1:	
+					if i == 1:
 						tacotwo.append(menuItems[(i-1)])
 					else:
 						tacotwo.append(menuItems[int((i-1)/2)])
@@ -107,8 +105,7 @@ for (i, c) in enumerate(questionCnts):
 					sides.append(menuSides[i-dif-1])
 				else:
 					sides.append(menuSides[i-dif])
-			
+
 print(tacoone)
 print(tacotwo)
 print(sides)
-
